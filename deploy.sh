b36e4466ffdde3bbf3b37166ec607b04c1f69beb
bross2005 <brossi2008@gmail.com>
2020-10-18 00:47:48 -0700

    second change for test

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 npm install -g serverless
 serverless config credentials --provider aws --key AKIAQWCI3DOZYYRSVZPD --secret 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
+npm install --silent --no-progress
 sls deploy
