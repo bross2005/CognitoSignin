f7ccb2a931caea662e448bc411c2399eb2986ad4
bross2005 <brossi2008@gmail.com>
2020-10-20 09:03:12 -0700

    update new things

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 npm install -g serverless
-serverless config credentials --provider aws --key AKIAQWCI3DOZYYRSVZPD --secret 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
 npm install --silent --no-progress
 sls deploy
